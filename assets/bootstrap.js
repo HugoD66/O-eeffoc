7604995bd6ff344ac9fd561df183ef4bf6173179
HugoD66 <dessauw.hugo@gmail.com>
2023-03-14 12:29:16 +0100

    correction bugs

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -6,12 +6,6 @@ export const app = startStimulusApp(require.context(
     true,
     /\.[jt]sx?$/
 ));
-import LiveController from '@symfony/ux-live-component';
-import '@symfony/ux-live-component/styles/live.css';
-
-
-app.register('live', LiveController);
-
 
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
